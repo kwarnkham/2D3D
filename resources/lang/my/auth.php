b87ee99d84f8d2a9eed74ab8b592488ce69fa92d
Moon <kwarnkham.moon@gmail.com>
2022-04-09 12:16:02 +0630

    start localization

---
+++ resources/lang/my/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'အချက်အလက်များမမှန်ပါ',
+    'password' => 'စကားဝှက်မမှန်ပါ',
+    'throttle' => 'နောက် :seconds စက္ကန့်မှ ထပ်မှန်ကြိုးစားပါ',
+
+];
---
+++ resources/lang/my/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'စကားဝှက်ကို ပြန်လည်သတ်မှတ်လိုက်ပါပြီ!',
+    'sent' => 'စကားဝှက်ကို ပြန်လည်သတ်မှတ်ရန် အီးမေးလ် ပို့လိုက်ပါပြီ!',
+    'throttled' => 'ခဏစောင့်ဆိုင်းပြီးမှ ထပ်မံကြိုးစားပါ',
+    'token' => 'စကားဝှက်ကို ပြန်လည်သတ်မှတ်သောတိုကင် မမှန်ပါ',
+    'user' => "ထိုအီးမေးလ်ဖြင့်လုပ်ထားသော အကောင့်ကိုရှာမတွေ့ပါ",
+
+];
