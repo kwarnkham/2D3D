6d37b88b8cbf3c29b8034cf1a7bbe9c1d163b027
Moon <kwarnkham.moon@gmail.com>
2022-04-25 13:30:13 +0630

    set bot command

---
+++ app/Console/Commands/SetBot.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class SetBot extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'set:bot';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Setup the bot';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $token = env("TELEGRAM_BOT_TOKEN");
+        $respobnse = Http::post("https://api.telegram.org/bot$token/setWebhook?url=https://c676-103-215-193-85.ngrok.io/api/$token");
+        echo $respobnse->body();
+    }
+}
