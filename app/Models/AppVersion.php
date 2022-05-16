a0b80bb6ffd64d524c14a2205e7421e1bca84f9d
Moon <kwarnkham.moon@gmail.com>
2022-05-16 17:41:51 +0630

    append telegram bot to app version

--- app/Models/AppVersion.php
+++ app/Models/AppVersion.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use App\Services\TelegramService;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class AppVersion extends AppModel
 {
     use HasFactory;
+
+
+    protected $appends = ['telegram_bot'];
+    public function telegramBot(): Attribute
+    {
+        return new Attribute(
+            get: fn () => TelegramService::getLink()
+        );
+    }
 }
