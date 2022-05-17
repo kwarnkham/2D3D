ccc7c1a34117df767ab4a63e65e1e073a35fe644
Moon <kwarnkham.moon@gmail.com>
2022-05-17 23:30:31 +0630

    update translated messages

--- resources/lang/my/messages.php
+++ resources/lang/my/messages.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'default message' => "':appName' မှကြိုဆိုပါသည်။ <a href=':appApk'>ဆော့ဝဲ</a> ဒေါင်းရန်။ <a href=':appClient'>တိုက်ရိုက်</a> သုံးရန်။ ",
+    'default message' => "':appName' မှကြိုဆိုပါသည်။ <a href=':appApk'>ဆော့ဝဲ android</a> ဒေါင်းရန်။ <a href=':appClient'>တိုက်ရိုက်</a> သုံးရန်။ ",
     'The followings are username and password.' => "Username နှင့် password သည်  အောက်ပါအတိုင်းဖြစ်သည်။ ",
     "is your username" => ":username ကသင့် username ဖြစ်သည်။ ",
     "forgot password" => "Password မေ့သွားသည်",
