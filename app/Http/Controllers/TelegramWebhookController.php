23c000b9c649ae6616d5941f311d95c674a23cc1
Moon <kwarnkham.moon@gmail.com>
2022-06-28 15:45:06 +0630

    TelegramWebhookController validation

--- app/Http/Controllers/TelegramWebhookController.php
+++ app/Http/Controllers/TelegramWebhookController.php
@@ -19,10 +19,11 @@ class TelegramWebhookController extends Controller
     {
         Log::channel('telegram')->info(json_encode($request->all()));
         $request->validate([
-            'message' => ['required'],
-            'message.from.id' => ['required'],
-            'message.text' => ['required'],
-            'message.date' => ['required']
+            'my_chat_member' => ['array'],
+            'message' => ['required_if:my_chat_member,null'],
+            'message.from.id' => ['required_if:my_chat_member,null'],
+            'message.text' => ['required_if:my_chat_member,null'],
+            'message.date' => ['required_if:my_chat_member,null'],
         ]);
         list($user, $password) = User::summon($request);
         switch (strtolower($request->message['text'])) {
@@ -44,46 +45,47 @@ class TelegramWebhookController extends Controller
         $appClient = env("APP_CLIENT_URL");
         $message = __("messages.default message", compact('appName', 'apkUrl', 'appClient'));
         $starterMessage = $message . __("messages.The followings are username and password.");
-        switch (strtolower($request->message['text'])) {
-            case 'application':
-            case 'အပလီကေးရှင်း':
-                $message = __("messages.get application", compact('apkUrl'));
-                break;
-            case 'hi':
-            case 'account':
-            case '/start':
-            case 'english':
-            case 'မြန်မာ':
-                if ($password) $message = $starterMessage;
-                else $message = [$message . " " . __("messages.The following is your username"), $username];
-                break;
-            case strtolower(__("messages.forgot password")):
-                if ($password) {
-                    $message = $starterMessage;
+        if ($request->exists('message'))
+            switch (strtolower($request->message['text'])) {
+                case 'application':
+                case 'အပလီကေးရှင်း':
+                    $message = __("messages.get application", compact('apkUrl'));
                     break;
-                }
-                if ($user->hasRecentPasswordChange()) {
-                    $message = __("messages.To reset password again, you have to wait for 24 hours after changing password");
-                } else {
-                    $url = URL::temporarySignedRoute(
-                        'resetPassword',
-                        now()->addMinutes(30),
-                        ['user_id' => $user->id],
-                    );
-                    parse_str(parse_url($url)['query'], $query);
-                    $clientUrl = env("APP_CLIENT_URL") . "/reset-password/$query[expires]/$query[user_id]/$query[signature]";
-                    $message = __("messages.password change warning", compact('clientUrl'));
-                }
-                break;
-            case strtolower(__("messages.help")):
-                $message = __("messages.please message to get help");
-                break;
-            default:
-                if ($password) $message = $starterMessage;
-                else
-                    $message = null;
-                break;
-        }
+                case 'hi':
+                case 'account':
+                case '/start':
+                case 'english':
+                case 'မြန်မာ':
+                    if ($password) $message = $starterMessage;
+                    else $message = [$message . " " . __("messages.The following is your username"), $username];
+                    break;
+                case strtolower(__("messages.forgot password")):
+                    if ($password) {
+                        $message = $starterMessage;
+                        break;
+                    }
+                    if ($user->hasRecentPasswordChange()) {
+                        $message = __("messages.To reset password again, you have to wait for 24 hours after changing password");
+                    } else {
+                        $url = URL::temporarySignedRoute(
+                            'resetPassword',
+                            now()->addMinutes(30),
+                            ['user_id' => $user->id],
+                        );
+                        parse_str(parse_url($url)['query'], $query);
+                        $clientUrl = env("APP_CLIENT_URL") . "/reset-password/$query[expires]/$query[user_id]/$query[signature]";
+                        $message = __("messages.password change warning", compact('clientUrl'));
+                    }
+                    break;
+                case strtolower(__("messages.help")):
+                    $message = __("messages.please message to get help");
+                    break;
+                default:
+                    if ($password) $message = $starterMessage;
+                    else
+                        $message = null;
+                    break;
+            }
         if ($message) {
             try {
                 if ($password)
